af7dcdb1bb80b7e283725068d7dad860c06808ab
binaryck <70761137+binaryck@users.noreply.github.com>
2025-08-10 15:11:15 +0200

    Order interface

--- src/popup/types/order.ts
+++ src/popup/types/order.ts
@@ -1,4 +1,22 @@
 // To complete
 export interface Order {
-  id: string;
+  status?: string;
+  charge?: {
+    id: string; // Order ID
+    address: string; // Bitcoin address to pay
+    amount: number; // in satoshis
+    lightning_invoice: {
+      expires_at: number; // timestamp in ms
+      payreq: string;
+    };
+    created_at: number; // timestamp in ms
+  };
+  paylink?: {
+    id: string; // visit https://app.hel.io/pay/66xxx16 to pay
+  };
+  chainFee?: number; // in satoshis
+  serviceFee?: number; // in satoshis
+  orderType?: string;
+  nonStandardTx?: boolean; //  400kb-4MB transaction, reveal tx will not be visible in public mempool
+  createdAt?: number; // timestamp in ms,
 }
