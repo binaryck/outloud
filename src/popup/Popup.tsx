9769b5411f373c7ab72534639a6764d0b7092e37
binaryck <70761137+binaryck@users.noreply.github.com>
2025-08-12 20:56:52 +0200

    Add chakra provider

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { PopupContent } from "./components/PopupContent/PopupContent";
 import { ServiceProvider } from "./context/serviceContext";
+import { Provider } from "@/components/ui/provider";
 
 export function Popup(): React.JSX.Element {
   return (
-    <ServiceProvider>
-      <div className="container-popup">
-        <MemoryRouter initialEntries={["/"]}>
-          <PopupContent />
-        </MemoryRouter>
-      </div>
-    </ServiceProvider>
+    <Provider>
+      <ServiceProvider>
+        <div className="container-popup">
+          <MemoryRouter initialEntries={["/"]}>
+            <PopupContent />
+          </MemoryRouter>
+        </div>
+      </ServiceProvider>
+    </Provider>
   );
 }
 
