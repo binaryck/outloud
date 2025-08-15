b8da0623ba8518969175b4a90f39da521224d491
binaryck <70761137+binaryck@users.noreply.github.com>
2025-08-15 14:07:07 +0200

    Add props to BackArrow component

--- src/popup/components/BackArrow/BackArrow.tsx
+++ src/popup/components/BackArrow/BackArrow.tsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
 
-export function BackArrow(): React.JSX.Element {
+interface BackArrowProps {
+  size?: number;
+  strokeWidth?: number;
+}
+
+export function BackArrow({
+  size = 25,
+  strokeWidth = 4,
+}: BackArrowProps): React.JSX.Element {
   return (
     <Link to="/" className="back-arrow">
-      <ArrowLeft size={25} strokeWidth={4} />
+      <ArrowLeft size={size} strokeWidth={strokeWidth} />
     </Link>
   );
 }
