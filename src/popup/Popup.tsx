4db9bb85037ef5c3b4cdb27c0c789746f38a4fe1
binaryck <70761137+binaryck@users.noreply.github.com>
2025-09-08 20:32:15 +0200

    Remove Chakra UI and its provider/toaster/tooltip

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -2,19 +2,16 @@ import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { PopupContent } from "./components/PopupContent/PopupContent";
 import { ServiceProvider } from "./context/serviceContext";
-import { Provider } from "@/components/ui/provider";
 
 export function Popup(): React.JSX.Element {
   return (
-    <Provider>
-      <ServiceProvider>
-        <div className="container-popup">
-          <MemoryRouter initialEntries={["/"]}>
-            <PopupContent />
-          </MemoryRouter>
-        </div>
-      </ServiceProvider>
-    </Provider>
+    <ServiceProvider>
+      <div className="container-popup">
+        <MemoryRouter initialEntries={["/"]}>
+          <PopupContent />
+        </MemoryRouter>
+      </div>
+    </ServiceProvider>
   );
 }
 
